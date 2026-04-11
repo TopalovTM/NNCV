c568961ecd19eef1c4284dfa895535974b66eee8
Tsvetan Topalov <scur2427@int6.local.snellius.surf.nl>
2026-04-11 19:23:56 +0200

    deeplabv3 first snellius run

--- final_assignment/scripts/download_docker_and_data.sh
+++ final_assignment/scripts/download_docker_and_data.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+#SBATCH --job-name=download_cityscapes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_mig
-#SBATCH --time=3:00:00
+#SBATCH --partition=gpu_a100
+#SBATCH --time=03:00:00
+#SBATCH --output=logs/%j_download.out
+#SBATCH --error=logs/%j_download.err
 
-apptainer pull container.sif docker://cclaess/5lsm0:v1
+set -e
 
+PROJECT_ROOT="$HOME/5LSH0/NNCV"
+cd "$PROJECT_ROOT"
+
+mkdir -p logs
 mkdir -p data
 
-apptainer exec --env-file .env container.sif /bin/bash -lc '
+if [ ! -f container.sif ]; then
+    apptainer pull container.sif docker://cclaess/5lsm0:v1
+fi
+
+apptainer exec --env-file final_assignment/.env container.sif /bin/bash -lc '
     huggingface-cli download TimJaspersTue/5LSM0 \
         --local-dir ./data \
         --repo-type dataset
--- final_assignment/scripts/train_snellius.sh
+++ final_assignment/scripts/train_snellius.sh
@@ -5,11 +5,14 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
 
 set -e
+
+PROJECT_ROOT="$HOME/5LSH0/NNCV/final_assignment"
+cd "$PROJECT_ROOT"
 
 mkdir -p logs
 
@@ -17,10 +20,10 @@ module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 module load CUDA/12.1.1
 
-python3 -m venv .venv
 source .venv/bin/activate
 
-pip install --upgrade pip
-pip install -r requirements.txt
+set -a
+source .env
+set +a
 
 python train.py --config configs/peak_deeplabv3plus_r50.yaml
