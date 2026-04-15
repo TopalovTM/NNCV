ebaa8079adc0abe3c1b40132b07a4cc12cd9cae0
Tsvetan Topalov <scur2427@int6.local.snellius.surf.nl>
2026-04-15 22:39:25 +0200

    evaluate tuned models

--- final_assignment/scripts/evaluate_snellius.sh
+++ final_assignment/scripts/evaluate_snellius.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=eval_r50
+#SBATCH --job-name=eval_r101_tuned
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -23,7 +23,7 @@ module load CUDA/12.1.1
 source .venv/bin/activate
 
 python evaluate.py \
-  --config configs/peak_deeplabv3plus_r50_metrics.yaml \
-  --checkpoint checkpoints/peak-deeplabv3plus-r50-metrics/best_model_loss.pt \
-  --save-dir evaluation_outputs/run2_r50_loss \
+  --config configs/peak_deeplabv3plus_r101_tuned.yaml \
+  --checkpoint checkpoints/peak-deeplabv3plus-r101-tuned/best_model_dice.pt \
+  --save-dir evaluation_outputs/run_r101_tuned_dice \
   --num-samples 8
